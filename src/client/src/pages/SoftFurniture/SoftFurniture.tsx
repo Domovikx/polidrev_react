6c9c49071fa1e30d4b1c60a0ec90edca1a71cc35
domovikx <domovikx@gmail.com>
2020-12-17 12:09:00 +0300

    chore: implement ScrollableTabs

--- src/client/src/pages/SoftFurniture/SoftFurniture.tsx
+++ src/client/src/pages/SoftFurniture/SoftFurniture.tsx
@@ -1,63 +1,12 @@
 import * as React from 'react';
 import CommonFade from '../../components/common/CommonFade';
-import SwipeableViews from 'react-swipeable-views';
+import ScrollableTabs from '../../components/common/ScrollableTabs';
+import { Box } from '@material-ui/core';
 import { imgPr2 } from '../../assets/Images';
 import { useStyles } from './SoftFurniture.styles';
-import {
-  Box,
-  Container,
-  Tab,
-  Tabs,
-  Typography,
-  useTheme,
-} from '@material-ui/core';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  index: any;
-  value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: any) {
-  return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
 
 export const SoftFurniture = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
-  };
 
   return (
     <CommonFade>
@@ -67,36 +16,7 @@ export const SoftFurniture = () => {
           style={{ backgroundImage: `url(${imgPr2})` }}
         />
 
-        <Container>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            variant="fullWidth"
-            aria-label="full width tabs example"
-          >
-            <Tab label="Item One" {...a11yProps(0)} />
-            <Tab label="Item Two" {...a11yProps(1)} />
-            <Tab label="Item Three" {...a11yProps(2)} />
-          </Tabs>
-
-          <SwipeableViews
-            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-            index={value}
-            onChangeIndex={handleChangeIndex}
-          >
-            <TabPanel value={value} index={0} dir={theme.direction}>
-              Item One
-            </TabPanel>
-            <TabPanel value={value} index={1} dir={theme.direction}>
-              Item Two
-            </TabPanel>
-            <TabPanel value={value} index={2} dir={theme.direction}>
-              Item Three
-            </TabPanel>
-          </SwipeableViews>
-        </Container>
+        <ScrollableTabs />
       </Box>
     </CommonFade>
   );
