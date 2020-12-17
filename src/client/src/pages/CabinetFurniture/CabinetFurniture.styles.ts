bb7748354c6d21ad37856eb0e3a1f8d28028c2aa
domovikx <domovikx@gmail.com>
2020-12-17 08:56:15 +0300

    chore: CabinetFurniture done

--- src/client/src/pages/CabinetFurniture/CabinetFurniture.styles.ts
+++ src/client/src/pages/CabinetFurniture/CabinetFurniture.styles.ts
@@ -8,4 +8,9 @@ export const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     boxShadow: `0px 50px 50px 0px white inset, ${theme.shadows[3]}`,
   },
+  list: {
+    display: 'grid',
+    gridTemplateColumns: `repeat(auto-fill, minmax(250px, 1fr))`,
+    margin: `15px 0`,
+  },
 }));
